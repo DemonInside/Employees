7b17a30af53e6715483e4402b6336779b71ee438
Dima <dmalkki@gmail.com>
2016-12-18 19:34:06 +0700

    added creation of default list with employees and output into console for it;

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -1,9 +1,26 @@
-﻿namespace Employees
+﻿using System;
+using System.Collections.Generic;
+
+namespace Employees
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var employeesService = new EmployeesService();
+
+            List<Employee> employees = employeesService.CreateDefaultEmployeesList();
+            ShowEmployeesData(employees);
+
+            Console.ReadLine();
+        }
+
+        private static void ShowEmployeesData(List<Employee> employees)
+        {
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"Id: {employee.Id} Name: {employee.Name} Salary: {employee.Salary.GetAverageMonthlySalary()}");
+            }
         }
     }
 }
