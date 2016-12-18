70f8480175c8a224760f29018977c25b660a4efc
Dima <dmalkki@gmail.com>
2016-12-18 19:37:38 +0700

    changed default employees names on english, fixed identifiers of default employees;

--- Employees/EmployeesService.cs
+++ Employees/EmployeesService.cs
@@ -9,10 +9,10 @@ namespace Employees
         {
             return new List<Employee>
             {
-                new Employee { Id = 0, Name = "Василий", Salary = new FixedMonthlySalary { SalaryPerMonth = 30000m } },
-                new Employee { Id = 0, Name = "Анна", Salary = new FixedMonthlySalary { SalaryPerMonth = 30000m } },
-                new Employee { Id = 0, Name = "Анатолий", Salary = new HourlySalary { SalaryPerHour = 210.34m } },
-                new Employee { Id = 0, Name = "Иван", Salary = new HourlySalary { SalaryPerHour = 150.24m } }
+                new Employee { Id = 0, Name = "Rick", Salary = new FixedMonthlySalary { SalaryPerMonth = 30000m } },
+                new Employee { Id = 1, Name = "Jhon", Salary = new FixedMonthlySalary { SalaryPerMonth = 30000m } },
+                new Employee { Id = 2, Name = "Jack", Salary = new HourlySalary { SalaryPerHour = 210.34m } },
+                new Employee { Id = 3, Name = "Sam", Salary = new HourlySalary { SalaryPerHour = 150.24m } }
             };
         }
     }
