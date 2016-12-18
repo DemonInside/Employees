9870262b520ac48828e2f8b33f5c687f947a738a
Dima <dmalkki@gmail.com>
2016-12-18 19:33:13 +0700

    added service class for employees with method for default employees list creation;

---
+++ Employees/EmployeesService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Employees.Salaries;
+
+namespace Employees
+{
+    public class EmployeesService
+    {
+        public List<Employee> CreateDefaultEmployeesList()
+        {
+            return new List<Employee>
+            {
+                new Employee { Id = 0, Name = "Василий", Salary = new FixedMonthlySalary { SalaryPerMonth = 30000m } },
+                new Employee { Id = 0, Name = "Анна", Salary = new FixedMonthlySalary { SalaryPerMonth = 30000m } },
+                new Employee { Id = 0, Name = "Анатолий", Salary = new HourlySalary { SalaryPerHour = 210.34m } },
+                new Employee { Id = 0, Name = "Иван", Salary = new HourlySalary { SalaryPerHour = 150.24m } }
+            };
+        }
+    }
+}
