e92d06bd78b0824e5e9b3d61bd0aaec6e8a11d23
Dima <dmalkki@gmail.com>
2016-12-18 19:26:07 +0700

    added class with constants for work info;

---
+++ Employees/WorkConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace Employees
+{
+    public static class WorkConsts
+    {
+        public const double AverageWorkingDaysInMonth = 20.8;
+        public const double WorkingHoursInDay = 8;
+    }
+}
